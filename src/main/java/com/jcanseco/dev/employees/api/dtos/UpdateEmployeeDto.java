5d932f16085c36c231e60615fe54693e79e15d38
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-06-10 16:55:11 -0600

    Update Employee Integration Tests

--- src/main/java/com/jcanseco/dev/employees/api/dtos/UpdateEmployeeDto.java
+++ src/main/java/com/jcanseco/dev/employees/api/dtos/UpdateEmployeeDto.java
@@ -1,5 +1,7 @@
 package com.jcanseco.dev.employees.api.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateEmployeeDto {
+    @Min(1)
     private Long employeeId;
+    @NotEmpty
     private String firstname;
+    @NotEmpty
     private String lastname;
 }
--- src/test/java/com/jcanseco/dev/employees/api/integration/UpdateEmployeeTests.java
+++ src/test/java/com/jcanseco/dev/employees/api/integration/UpdateEmployeeTests.java
@@ -1,8 +1,88 @@
 package com.jcanseco.dev.employees.api.integration;
 
 import com.jcanseco.dev.employees.api.base.MySQLTestBase;
+import com.jcanseco.dev.employees.api.dtos.UpdateEmployeeDto;
+import com.jcanseco.dev.employees.api.entities.Employee;
+import com.jcanseco.dev.employees.api.repositories.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.hamcrest.Matchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UpdateEmployeeTests extends MySQLTestBase {
 
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private EmployeeRepository repository;
+    private Employee employee;
+    @BeforeEach
+    public void setup() {
+        employee = repository.saveAndFlush(
+                Employee.builder().firstname("John").lastname("Doe").build()
+        );
+    }
+    @AfterEach
+    public void cleanup() {
+        repository.deleteAll();
+    }
+
+    @Test
+    public void updateEmployeeStatusShouldBeOk() throws Exception {
+
+        var employeeId = employee.getId();
+        var updateDto = new UpdateEmployeeDto(employeeId, "John Maximum", "Dax Doe");
+
+        var request = MockMvcRequestBuilders
+                .put("/employees/" + employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updateDto));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.notNullValue()))
+                .andExpect(jsonPath("$.firstname").value(updateDto.getFirstname()))
+                .andExpect(jsonPath("$.lastname").value(updateDto.getLastname()));
+    }
+
+    @Test
+    public void updateEmployeeWhenModelHasErrorStatusShouldBeBadRequest() throws Exception {
+        var employeeId = -1L;
+        var updateDto = new UpdateEmployeeDto(employeeId, "", "");
+
+        var request = MockMvcRequestBuilders
+                .put("/employees/" + employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updateDto));
+
+        mockMvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void updateEmployeeWhenEmployeesNotExistsStatusShouldBeNotFound() throws Exception {
+
+        var employeeId = 5000L;
+        var updateDto = new UpdateEmployeeDto(employeeId, "Not Found", "Doe");
+
+        var request = MockMvcRequestBuilders
+                .put("/employees/" + employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(updateDto));
+
+        mockMvc.perform(request)
+                .andExpect(status().isNotFound());
+    }
 }
