e1108fea78bad8976b118c64ea84555d5d2e6fa4
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-06-13 06:44:21 -0600

    Add command line runner

--- src/main/java/com/jcanseco/dev/employees/api/EmployeesApiApplication.java
+++ src/main/java/com/jcanseco/dev/employees/api/EmployeesApiApplication.java
@@ -1,13 +1,30 @@
 package com.jcanseco.dev.employees.api;
 
+import com.jcanseco.dev.employees.api.entities.Employee;
+import com.jcanseco.dev.employees.api.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class EmployeesApiApplication {
+public class EmployeesApiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeesApiApplication.class, args);
     }
 
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.saveAllAndFlush(
+                List.of(
+                   Employee.builder().firstname("John").lastname("Doe").build(),
+                   Employee.builder().firstname("Jane").lastname("Doe").build()
+                ));
+    }
 }
